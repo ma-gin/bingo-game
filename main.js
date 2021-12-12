6ee3b9891d19f8d5c7f9fc86692813f28639c03b
Matt Ginter <mattginter@gmail.com>
2021-12-12 23:30:23 +0100

    Add player board

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 let bingoCell = document.createElement("div")
 const numberOfCells = 76
+const playerBoardCells = 24
+const playerNumbers = []
 
 // ************    DECLARATION OF FUNCTIONS    ************
 
@@ -10,16 +12,16 @@ const createCell = () =>{
     bingoCell.classList.add("cell")
 }
 
-const createBingoBoard = () =>{
-    for (let cellNumber = 1; cellNumber <= numberOfCells; cellNumber++){
+const createMainBoard = (x) =>{
+    for (let cellNumber = 1; cellNumber <= x; cellNumber++){
         createCell()
         bingoCell.innerText = cellNumber
         document.getElementById("bingo-board-container").appendChild(bingoCell)
 }}
 
-const firstArray = () =>{
+const firstArray = (x) =>{
     const arr = []
-    for (let i = 1; i <= numberOfCells; i++){
+    for (let i = 1; i <= x; i++){
         arr.push(i)}
     return arr
 }
@@ -27,13 +29,12 @@ const firstArray = () =>{
 const chooseWinner = (range) =>{
     const randomIndex = Math.floor(Math.random() * range.length)
     const winningNumber = numberArray.splice(randomIndex, 1)[0]
-    // console.log(winningNumber)
     return winningNumber
 }
 
 const clickPlay = () =>{
     if (numberArray.length === 0){
-        alert("This Shouldn't Happen.")
+        alert("Game Over.")
         return
     } else{
     const allCells = document.querySelectorAll("main div")
@@ -44,7 +45,45 @@ const clickPlay = () =>{
             testCell.classList.add("winningNumber")
 }}}}
 
+const playerNumberCell = (arr) =>{ //output a number between 1 and 76 x amount of times without repeating the number
+    const randomIndex = Math.floor(Math.random() * arr.length)
+    const newArr = arr
+    const winningNumber = newArr.splice(randomIndex, 1)[0]
+    return winningNumber
+}
+
+const createPlayerBoard = (cells, boards) =>{
+    const playerNumberArray = firstArray(numberOfCells)
+    const playerSection = document.createElement("section")
+    // console.log("you are here")
+    if (boards === undefined){
+        const boardContainer = document.createElement("div")
+        boardContainer.classList.add("player-board")
+        for (let i = 0; i < cells; i++){
+            // console.log("here too")
+            createCell()
+            bingoCell.classList.remove("cell")
+            bingoCell.classList.add("player-cell")
+            bingoCell.innerText = playerNumberCell(playerNumberArray)
+            boardContainer.appendChild(bingoCell)
+            // console.log(boardContainer)
+    //     }} else {
+    //         for(let i = 0; i < boards; i++){
+    //             const boardContainer = document.createElement("div")
+    //             for (let i = 0; i < cells; i++){
+    //                 createCell()
+    //                 bingoCell.classList.remove("cell")
+    //                 bingoCell.classList.add("player-cell")
+    //                 bingoCell.innerText = playerNumberCell(playerNumberArray)
+    //                 boardContainer.appendChild(bingoCell)
+    //                 boardContainer.classList.add("player-board")}       
+    // }}
+    playerSection.appendChild(boardContainer)
+    document.getElementsByTagName("body")[0].appendChild(playerSection)
+}}}
+
 // ************    INITIALIZATION    ************
 
-createBingoBoard()
-const numberArray = firstArray()
+createMainBoard(numberOfCells)
+const numberArray = firstArray(numberOfCells)
+createPlayerBoard(playerBoardCells)
