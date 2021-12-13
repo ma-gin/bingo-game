13c750e88bcc934e424f98ec9581aee38c47889c
Matt Ginter <mattginter@gmail.com>
2021-12-13 23:32:39 +0100

    Add player amount

--- main.js
+++ main.js
@@ -38,17 +38,27 @@ const chooseWinner = (range) =>{
 }
 
 const clickPlay = () =>{
+    const playerAmount = document.getElementById("players").value
     if (numberArray.length === 0){
         alert("Game Over. Please Refresh.")
-        return
-    } else{
+        return} 
+    else{
     const allCells = document.querySelectorAll("main div")
     const winner = chooseWinner(numberArray).toString()
     for (let i = 0; i < allCells.length; i++){
         const testCell = allCells[i]
         if (testCell.innerText === winner){
-            testCell.classList.add("winningNumber")
-}}}}
+            testCell.classList.add("winningNumber")}}
+    for (let i = 0; i < parseInt(playerAmount); i++ ){
+        const playerBoard = document.querySelectorAll(".player-section #player-board div")
+        console.log(playerBoard)
+//         for (let j = 0; j < playerBoard.length; i++){
+//         const checkCell = playerBoard[j]
+//             if (checkCell.innerText === winner){
+//                 checkCell.classList.add("winningNumber")
+// }}
+}}}
+
 
 const playerNumberCell = (arr) =>{ //output a number between 1 and 76 x amount of times without repeating the number
     const randomIndex = Math.floor(Math.random() * arr.length)
@@ -63,41 +73,32 @@ const createPlayerBoard = (cells, boards) =>{
     playerSection.classList.add("player-section")
     let boardContainer = document.createElement("div")
     let freeCell
-    if (boards === undefined){
+    for (let i = 0; i < boards; i++){
         playerNumberArray = fillArray(numberOfCells)
+        boardContainer = document.createElement("div")
         for (let i = 0; i < cells; i++){
             createCell()
             bingoCell.classList.remove("cell")
             bingoCell.classList.add("player-cell")
             bingoCell.innerText = playerNumberCell(playerNumberArray)
             boardContainer.appendChild(bingoCell)}
-        boardContainer.id = "player-board"} 
-    else{
-        for (let i = 0; i < boards; i++){
-            playerNumberArray = fillArray(numberOfCells)
-            boardContainer = document.createElement("div")
-            for (let i = 0; i < cells; i++){
-                createCell()
-                bingoCell.classList.remove("cell")
-                bingoCell.classList.add("player-cell")
-                bingoCell.innerText = playerNumberCell(playerNumberArray)
-                boardContainer.appendChild(bingoCell)}
-            boardContainer.id = "player-board"
-            freeCell = boardContainer.children.item(12)
-            freeCell.innerText = "X"
-            freeCell.classList.add("free")
-            playerSection.appendChild(boardContainer)}
-        document.getElementsByTagName("body")[0].appendChild(playerSection)
-        return}
-    freeCell = boardContainer.children.item(12)
-    freeCell.innerText = "X"
-    freeCell.classList.add("free")
-    playerSection.appendChild(boardContainer)
+        boardContainer.id = "player-board"
+        freeCell = boardContainer.children.item(12)
+        freeCell.innerText = "X"
+        freeCell.classList.add("free")
+        playerSection.appendChild(boardContainer)}
     document.getElementsByTagName("body")[0].appendChild(playerSection)
 }
 
+const startGame = () =>{
+    const playerAmount = document.getElementById("players").value
+    const startDiv = document.getElementById("start-div")
+    const playBtn = document.getElementById("play-button")
+    createPlayerBoard(playerBoardCells, parseInt(playerAmount))
+    startDiv.classList.add("hide")
+    playBtn.classList.remove("hide")
+}
 // ************    INITIALIZATION    ************
 
 createMainBoard(numberOfCells)
 const numberArray = fillArray(numberOfCells)
-createPlayerBoard(playerBoardCells, 5)
