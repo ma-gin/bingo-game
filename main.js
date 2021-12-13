3753d0be0b78a2c5755e368e99e0a7a06484fc7d
Matt Ginter <mattginter@gmail.com>
2021-12-13 19:47:54 +0100

    Add free cell

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ************    DECLARATION OF VARIABLES    ************
 
 let bingoCell = document.createElement("div")
-const numberOfCells = 76
-const playerBoardCells = 24
+const numberOfCells = 75
+const playerBoardCells = 25
 const playerNumbers = []
 
 // ************    DECLARATION OF FUNCTIONS    ************
@@ -52,12 +52,18 @@ const playerNumberCell = (arr) =>{ //output a number between 1 and 76 x amount o
     return winningNumber
 }
 
+const makeCenterFree = () =>{
+    const allCells = document.getElementById("player-board-id").children
+    allCells.item(13).classList.add("free")
+}
+
 const createPlayerBoard = (cells, boards) =>{
     const playerNumberArray = firstArray(numberOfCells)
     const playerSection = document.createElement("section")
+    playerSection.classList.add("player-section")
+    const boardContainer = document.createElement("div")
     // console.log("you are here")
     if (boards === undefined){
-        const boardContainer = document.createElement("div")
         boardContainer.classList.add("player-board")
         for (let i = 0; i < cells; i++){
             // console.log("here too")
@@ -65,7 +71,13 @@ const createPlayerBoard = (cells, boards) =>{
             bingoCell.classList.remove("cell")
             bingoCell.classList.add("player-cell")
             bingoCell.innerText = playerNumberCell(playerNumberArray)
-            boardContainer.appendChild(bingoCell)
+            boardContainer.appendChild(bingoCell)}
+        boardContainer.id = "player-board-id"}
+    const freeCell = boardContainer.children.item(12)
+    freeCell.innerText = "X"
+    freeCell.classList.add("free")
+    playerSection.appendChild(boardContainer)
+    document.getElementsByTagName("body")[0].appendChild(playerSection)
             // console.log(boardContainer)
     //     }} else {
     //         for(let i = 0; i < boards; i++){
@@ -78,9 +90,8 @@ const createPlayerBoard = (cells, boards) =>{
     //                 boardContainer.appendChild(bingoCell)
     //                 boardContainer.classList.add("player-board")}       
     // }}
-    playerSection.appendChild(boardContainer)
-    document.getElementsByTagName("body")[0].appendChild(playerSection)
-}}}
+
+}
 
 // ************    INITIALIZATION    ************
 
